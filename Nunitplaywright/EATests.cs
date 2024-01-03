679bad62b34a874fe15dc60a7cf5cedd44d4205b
franklinjisno29 <Franklin.Jisno@ust.com>
2024-01-03 12:54:26 +0530

    3/1/24 class work

---
+++ Nunitplaywright/EATests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunitplaywright
+{
+    [TestFixture]
+    internal class EATests : PageTest
+    {
+        [SetUp]
+        public async Task Setup()
+        {
+            Console.WriteLine("Opened browser");
+            await Page.GotoAsync("http://eaapp.somee.com/");
+            Console.WriteLine("Page loaded");
+        }
+
+        [Test]
+        public async Task LoginTest()
+        {
+            //await Page.GetByText("Login").ClickAsync();
+            //var lnklogin = Page.Locator(selector: "text=Login");
+            //await lnklogin.ClickAsync();
+            await Page.ClickAsync(selector: "text=Login");
+            await Console.Out.WriteLineAsync("link clicked");
+
+            await Expect(Page).ToHaveURLAsync("http://eaapp.somee.com/Account/Login");
+
+            //await Page.GetByLabel("UserName").FillAsync(value: "admin");
+            //await Page.GetByLabel("Password").FillAsync(value: "password");
+
+            //await Page.Locator("#UserName").FillAsync(value: "admin");
+            //await Page.Locator("#Password").FillAsync(value: "password");
+
+            await Page.FillAsync(selector: "#UserName", value: "admin");
+            await Page.FillAsync(selector: "#Password", value: "password");
+            await Console.Out.WriteLineAsync("Typed");
+
+            //await Page.Locator("//input[@value='Log in']").ClickAsync();
+            var btnlogin = Page.Locator(selector: "input", new PageLocatorOptions{ HasTextString = "Log in"});
+            await btnlogin.ClickAsync();
+
+            await Console.Out.WriteLineAsync("clicked");
+            //await Expect(Page).ToHaveTitleAsync("Home - Execute Automation Employee App");
+            await Expect(Page.Locator(selector:"text='Hello admin!'")).ToBeVisibleAsync();
+
+        }
+    }
+}
